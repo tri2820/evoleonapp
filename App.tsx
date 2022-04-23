417b843a3e1398f548f13172251f3ab772ffe228
ahmedarif7 <maarif@deakin.edu.au>
2022-04-23 20:11:32 +0400

    Added signup form

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignupForm from './components/signupForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <SignupForm />
       <Text>Open up App.tsx to start working on your app! Testing</Text>
       <StatusBar style="auto" />
     </View>
---
+++ components/signupForm/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header() {
+    return(
+        <nav class="text-dark navbar">
+            <div className="row col-12 d-flex">
+                <h3>Registration</h3>
+            </div>
+        </nav>
+    )
+}
+export default Header;
